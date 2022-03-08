5a81e840a006e5179cc5098647d2af368b2916f7
s.seemann <s.seemann@adito.de>
2022-03-08 15:28:03 +0100

    feat: if thread is interrupted, the process is destroyed and the correct exit code will be returned

--- src/main/java/org/buildobjects/process/Proc.java
+++ src/main/java/org/buildobjects/process/Proc.java
@@ -77,28 +77,35 @@ class Proc implements EventSink {
             throw new StartupException("Could not startup process '" + toString() + "'.", e);
         }
 
-        try {
             startControlThread();
 
             do {
-                ExecutionEvent nextEvent = timeout == null ? eventQueue.poll(MAX_VALUE, HOURS) : eventQueue.poll(timeout, MILLISECONDS);
+                try {
+                    ExecutionEvent nextEvent = timeout == null ? eventQueue.poll(MAX_VALUE, HOURS) : eventQueue.poll(timeout, MILLISECONDS);
 
-                if (nextEvent == null) {
-                    killCleanUpAndThrowTimeoutException();
-                }
+                    if (nextEvent == null) {
+                        killCleanUpAndThrowTimeoutException();
+                    }
 
-                if (nextEvent == PROCESS_EXITED) {
-                    break;
-                }
+                    if (nextEvent == PROCESS_EXITED) {
+                        break;
+                    }
 
-                if (nextEvent == EXCEPTION_IN_STREAM_HANDLING) {
+                    if (nextEvent == EXCEPTION_IN_STREAM_HANDLING) {
+                        killProcessCleanup();
+                        break;
+                    }
+                } catch (InterruptedException e) {
+                    // if this thread is interrupted, the process is destroyed
+                    // the process gets an exit code, so the next event is PROCESS_EXITED
                     killProcessCleanup();
-                    break;
+                    continue;
                 }
 
                 throw new RuntimeException("Felix reckons we should never reach this point");
             } while (true);
 
+        try {
             List<Throwable> exceptions = ioHandler.joinConsumption();
             if (!exceptions.isEmpty()) {
                 throw new IllegalStateException("Exception in stream consumption", exceptions.get(0));
